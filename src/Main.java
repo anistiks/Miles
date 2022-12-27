674d8b6b6f9287844cad1f9f465ff9fa998389c7
test <anistiks@gmail.com>
2022-12-27 13:31:32 +0300

    amount

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+            int fly =  2000;
+            int  roubles = 20;
+            int amount = fly/roubles;
+
+            System.out.println("Количество миль:" + amount);
+    }
+}
